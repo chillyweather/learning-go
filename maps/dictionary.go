7f70f3b3792211751b4a8bfc7b39a830d54f69a2
Dmitri <d.dzmitryeu@gmail.com>
2024-11-01 11:10:01 +0200

    +

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,7 @@
 package maps
 
-func Search(dictionary map[string]string, word string) string {
-	return "something"
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	return d[word], nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,33 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+
+		_, err := dictionary.Search("unknown")
+		want := "could not find what you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
+		t.Errorf("\ngot   %q\nwant  %q", got, want)
 	}
 }
